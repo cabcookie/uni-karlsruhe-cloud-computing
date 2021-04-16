66bb76614e4bc8ae71f18a20ffc039076e3045a8
Carsten Koch <carsten.b.koch@gmail.com>
2021-04-16 17:12:42 +0200

    feat: stack doesn't depend on Route 53

--- iaas/static-website-hosting.ts
+++ iaas/static-website-hosting.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// import { HostedZone } from '@aws-cdk/aws-route53';
 import { CloudFrontWebDistribution, OriginAccessIdentity, SecurityPolicyProtocol, SSLMethod } from '@aws-cdk/aws-cloudfront';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
@@ -11,16 +10,11 @@ import { CfnOutput, RemovalPolicy, Stack } from '@aws-cdk/core';
  * The site redirects from HTTP to HTTPS, using a CloudFront distribution,
  * Route53 alias record, and ACM certificate.
  * 
- * This stack relies on getting the domain name from CDK context and 
- * subdomains for the environments `test` and `prod` and the current domain
- * it should be deployed in.
- * Use 'cdk synth -c env=test'
- * and add the following to cdk.json:
+ * This stack relies on getting the domain name from CDK context.
+ * Add the following to cdk.json:
  * {
  *   "context": {
  *     "domain": "mystaticsite.com",
- *     "subdomain-test": "xyz-test",
- *     "subdomain-prod": "xyz"
  *   }
  * }
  */
@@ -33,19 +27,12 @@ export class StaticWebsiteS3 extends Stack {
             bucketName: "unikarlsruhe-site-bucket",
             websiteIndexDocument: "index.html",
             websiteErrorDocument: "error.html",
-
-            // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-            // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
-            // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
             removalPolicy: RemovalPolicy.DESTROY,
         });
         new CfnOutput(this, "SiteBucketName", { value: siteBucket.bucketName });
         
-        const env = this.node.tryGetContext("env");
-        const domain = this.node.tryGetContext("domain");
-        const subdomain = this.node.tryGetContext(`subdomain-${env}`);
+        const siteDomain = this.node.tryGetContext("domain");
         const certificateArn = this.node.tryGetContext("certificateArn");
-        const siteDomain = `${subdomain}.${domain}`;
         
         // CloudFront distribution that provides HTTPS
         const oai = new OriginAccessIdentity(this, "OriginAccessIdentity");
@@ -64,7 +51,6 @@ export class StaticWebsiteS3 extends Stack {
                 securityPolicy: SecurityPolicyProtocol.TLS_V1_1_2016,
             },
         });
-        new CfnOutput(this, "SiteDistributionId", { value: distribution.distributionId });
         new CfnOutput(this, "SiteUrl", { value: `https://${siteDomain}` });
 
         // Deploy `./src` on your S3 Bucket
